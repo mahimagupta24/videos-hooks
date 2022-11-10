4de2ba472283396b66026c3cbe39ca7518d2d853
mahimagupta24 <mahimagupta2401@gmail.com>
2022-11-10 12:03:37 +0530

    refactor

--- src/apis/youtube.js
+++ src/apis/youtube.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const KEY='AIzaSyC0uiP7egnV8m8Jp2ml9jYPd_irXg-D6jI'
+// const KEY='AIzaSyC0uiP7egnV8m8Jp2ml9jYPd_irXg-D6jI'
+const KEY='AIzaSyBxhPYrWeaqBGzKAHq8NR2pvJk-42Ob2QE'
 export default axios.create({
     baseURL: 'https://www.googleapis.com/youtube/v3',
     params:{
